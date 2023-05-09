f1e02801fa3f0393eef89e424a16fe4d6ab4d11e
abutsev <69083681+abutsev@users.noreply.github.com>
2023-05-09 17:17:30 +0300

    Update App.jsx

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>УСАЧ КУПИ БИТКОИНЫ И ОПЛАТИ СВАДЬБУ</h1>
+      <h1>ГОГА ЗАШКВАРИЛСЯ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
