da917e4e215423ca5b376001d37b1041d1346d85
abutsev <69083681+abutsev@users.noreply.github.com>
2023-05-10 11:54:13 +0300

    Update App.jsx

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   return (
     <>
       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-
+/*        <a href="https://vitejs.dev" target="_blank">
+           <img src={viteLogo} className="logo" alt="Vite logo" />
+         </a>
+         <a href="https://react.dev" target="_blank">
+           <img src={reactLogo} className="logo react" alt="React logo" />
+         </a>
+*/
           <img src={gogaLogo} className="" alt="goga Logo" />
        
       </div>
