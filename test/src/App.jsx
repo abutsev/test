25150c956842f0d609d788be554fbff0cb4ca6e7
abutsev <69083681+abutsev@users.noreply.github.com>
2023-05-10 11:57:03 +0300

    Update App.jsx

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import gogaLogo from '/photo.jpg'
 import './App.css'
 
@@ -10,13 +9,7 @@ function App() {
   return (
     <>
       <div>
-/*        <a href="https://vitejs.dev" target="_blank">
-           <img src={viteLogo} className="logo" alt="Vite logo" />
-         </a>
-         <a href="https://react.dev" target="_blank">
-           <img src={reactLogo} className="logo react" alt="React logo" />
-         </a>
-*/
+
           <img src={gogaLogo} className="" alt="goga Logo" />
        
       </div>
